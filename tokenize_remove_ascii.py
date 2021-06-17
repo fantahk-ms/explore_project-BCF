34689abb10c8ab93e48ed8a20fbafa3d26e85756
fantahk <t-fkabba@microsoft.com>
2021-06-17 10:05:29 -0500

    trying to get rid of non-words and numbers

--- tokenize_remove_ascii.py
+++ tokenize_remove_ascii.py
@@ -2,6 +2,7 @@
 import transform
 import string
 from nltk import word_tokenize
+import re
 # from english_words import english_words_set
 # import re
 
@@ -11,14 +12,15 @@ from nltk import word_tokenize
 current_list = transform.data_list
 curr = [str(e) for e in current_list ]
 ascii_set = set(string.printable)
+# ascii_set = set(r'[\W_]+')
 # words = set(nltk.corpus.words.words())
 # d = english_words_set
 
 
-example = ['Mary had a little lamb None' , 
-           'Jack went up the hill Grand Total' , 
-           'Jill followed suit' ,'i woke up suddenly' ,
-           'it was a really bad dream...', '通过代理上限MDATP设备状态异常 通过代理上限MDATP设备状态异常 apple banana 1234', 'None', 'None Grand Total']
+example = ['Mary had a little lamb *** None' , 
+           'Jack went up the hill $#! Grand Total' , 
+           'Jill followed suit)*' ,'i woke^& up suddenly' ,
+           'it was a really bad dream...', '通过代理上限MDATP设备状态异常 通过代理上限MDATP设备状态异常 apple banana 1234', 'None', 'None Grand Total', '!@#$']
 
 # def remove_non_english(data):
     # words = set(nltk.corpus.words.words())
@@ -36,12 +38,28 @@ def remove_non_ascii(data):
 
 def tokenize(data):
   tokenized_sents = [word_tokenize(i) for i in data]
+  data = tokenized_sents
+ # return data
 
 def remove_extra(data):
-     bad_words = ['None', 'Grand Total']
-     for w in data:
-         if w in bad_words:
-             data.remove(w)
+   extra = ['None', 'Grand', 'Total']
+   tokens = [word_tokenize(i) for i in data]
+  #  filtered = []
+   for i in range(len(tokens)):
+       filtered = [t for t in tokens[i] if not t in extra]
+       # print(*filtered)
+       data[i] = filtered   
+      # print("\n")
+   # return filtered
+
+
+def remove_non_number_words(data):
+    data = [str(x) for x in data]
+    tokens = [word_tokenize(i) for i in data]
+    for i in range(len(tokens)):
+        y = str(tokens[i])
+        re.sub(r'[\W_]+', ' ', y)
+    
 
 
 tokenize(example)
@@ -49,6 +67,11 @@ tokenize(example)
 # remove_extra(curr)
 # print(curr)
 
+# remove_extra(example)
 example = remove_non_ascii(example)
 remove_extra(example)
+remove_non_number_words(example)
 print(example)
+
+# try switching order
+# try moving it into remove_ascii
